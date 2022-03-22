ee4bffe67ebce9ee132b06c3ae5ad1dfbe15ec83
Ball-nna <minasszz1234@gmail.com>
2022-03-22 15:52:18 +0700

    Finish v1

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error',(error)=> console.error(error))
-db.once('once',()=>console.log('Connected to Database'))
+db.once('open',()=>console.log('Connected to Database'))
+
+app.use(express.json())
+
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers',subscribersRouter)
 
 app.get("/",(req,res)=>{
     res.send("Hello Node.js REST Server")
